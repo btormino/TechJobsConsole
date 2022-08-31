1f2824b68d29eb5ef4451de8dba2dd65bcfeff36
Bethany <morphforlife@gmail.com>
2022-08-31 06:15:41 -0500

    findByColumnAndValue updated

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -47,9 +47,9 @@ namespace TechJobsConsole
 
             foreach (Dictionary<string, string> row in AllJobs)
             {
-                string aValue = row[column];
+                string aValue = row[column].ToUpper();
 
-                if (aValue.Contains(value))
+                if (aValue.Contains(value.ToUpper()))
                 {
                     jobs.Add(row);
                 }
